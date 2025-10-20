95a90706709468141d151518639c54f9ac5743bf
D4V1D <despinozab@unsa.edu.pe>
2025-10-20 10:24:48 -0500

    refactor: se convirtieron los modelos de asistencia a Django ORM

---
+++ app/models/asistencia/__init__.py
@@ -0,0 +1,7 @@
+from .asistencia import Asistencia
+from .estadoAsistencia import EstadoAsistencia
+
+__all__ = [
+    'Asistencia',
+    'EstadoAsistencia',
+]
--- app/models/asistencia/asistencia.py
+++ app/models/asistencia/asistencia.py
@@ -1,16 +1,33 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+from django.db import models
+from app.models.usuario.estudiante import Estudiante
+from app.models.curso.curso import Curso
+from app.models.asistencia.estadoAsistencia import EstadoAsistencia
 
-class Asistencia:
-    def __init__(self):
-        self.id = None
-        self.estudianteID = None
-        self.cursoID = None
-        self.fecha = None
-        self.estado = None
 
-    def marcarPresente(self, ):
+class Asistencia(models.Model):
+    """Modelo para el registro de asistencia"""
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE, related_name='asistencias')
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE, related_name='asistencias')
+    fecha = models.DateField()
+    hora = models.TimeField(auto_now_add=True)
+    estado = models.ForeignKey(EstadoAsistencia, on_delete=models.PROTECT)
+    observaciones = models.TextField(blank=True, null=True)
+    justificada = models.BooleanField(default=False)
+    
+    class Meta:
+        db_table = 'asistencia'
+        verbose_name = 'Asistencia'
+        verbose_name_plural = 'Asistencias'
+        unique_together = [['estudiante', 'curso', 'fecha']]
+    
+    def __str__(self):
+        return f"{self.estudiante.cui} - {self.curso.codigo} - {self.fecha}"
+    
+    def marcar_presente(self):
+        """Marcar presente"""
+        pass
+    
+    def marcar_falta(self):
+        """Marcar falta"""
         pass
 
-    def marcarFalta(self, ):
-        pass
