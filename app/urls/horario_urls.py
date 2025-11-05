fee60f9d5e55f7ea2481ef532948736a3d0703d8
MauriceCC <mcornejoalv@unsa.edu.pe>
2025-11-04 22:40:54 -0500

    Advance in Horarios

--- app/urls/horario_urls.py
+++ app/urls/horario_urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views.horario_view import HorarioUsuarioView
+from app.views.horario_view import HorarioUsuarioView
 
 urlpatterns = [
     path('horarios/', HorarioUsuarioView.as_view(), name='horarios_usuario'),
