a3daeca7cdd5f53448397d53834994e667f2c94a
Jesus <jquinteros@unsa.edu.pe>
2025-11-17 17:47:38 -0500

    nuevo model para soportar matriculas, se realizo migraciones

---
+++ app/models/matricula_horario/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class MatriculaHorarioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app.models.matricula_horario'
+    verbose_name = 'Matrícula por Horario'
